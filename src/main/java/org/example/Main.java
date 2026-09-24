9c849f812371e36f8b8161499de3f756428d4b5d
imroj5 <imroj4850@gmail.com>
2026-09-24 22:16:32 +0530

    just checking branches

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     static void main(String[] args) {
 
-        ApplicationContext context= new AnnotationConfigApplicationContext(AppConfig.class);
-OrederService orders =context.getBean(OrederService.class);
-     orders.orederService();
 
 
     }
